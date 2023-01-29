dc6531cda30900b9506669c0c5a66ed1cabc78fc
renankanu <renankanu@gmail.com>
2023-01-29 20:19:38 -0300

    SAVEPOINT

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:kn_fit_app/app/core/widgets/base_app_bar.dart';
+import 'package:kn_fit_app/app/core/core.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -8,16 +8,107 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: BaseAppBar(
-        title: 'Home',
-        isShowBackButton: false,
+    final statusBarHeight = MediaQuery.of(context).padding.top;
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.fromLTRB(20, 10 + statusBarHeight, 20, 0),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset(
+                AppImages.icNotification,
+                height: 24,
+                color: AppColors.tiara,
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: const [
+              Text(
+                'Olá,',
+                style: TextStyle(
+                  color: AppColors.tiara,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                'João',
+                style: TextStyle(
+                  color: AppColors.berylGreen,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              ItemHome(
+                label: 'Treinos',
+                icon: AppImages.icBarbell,
+              ),
+              ItemHome(
+                label: 'Perfil',
+                icon: AppImages.icUser,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              ItemHome(
+                label: 'Favoritos',
+                icon: AppImages.icFavorite,
+              ),
+              ItemHome(
+                label: 'Avaliações',
+                icon: AppImages.icNotification,
+              ),
+            ],
+          )
+        ],
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+    );
+  }
+}
+
+class ItemHome extends StatelessWidget {
+  const ItemHome({
+    super.key,
+    required this.label,
+    required this.icon,
+  });
+
+  final String label;
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 120,
+      height: 120,
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: AppColors.berylGreen),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(label, style: const TextStyle(color: AppColors.berylGreen)),
+          const SizedBox(height: 10),
+          Image.asset(
+            icon,
+            height: 24,
+            color: AppColors.berylGreen,
+          ),
+        ],
       ),
     );
   }
--- lib/app/theme/themes.dart
+++ lib/app/theme/themes.dart
@@ -6,6 +6,7 @@ import '../core/colors.dart';
 class AppTheme {
   AppTheme._();
   static final theme = ThemeData.dark().copyWith(
+    useMaterial3: true,
     primaryColor: AppColors.ebonyClay,
     visualDensity: VisualDensity.adaptivePlatformDensity,
     scaffoldBackgroundColor: Colors.black,
@@ -14,7 +15,73 @@ class AppTheme {
       secondary: AppColors.berylGreen,
       onSecondary: AppColors.berylGreen,
     ),
-    textTheme: GoogleFonts.exo2TextTheme(),
+    textTheme: GoogleFonts.exo2TextTheme().copyWith(
+      displayLarge: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+      ),
+      displayMedium: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+      ),
+      displaySmall: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 18,
+        fontWeight: FontWeight.w700,
+      ),
+      headlineMedium: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+      ),
+      headlineSmall: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+      ),
+      titleLarge: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+      ),
+      bodyLarge: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+      ),
+      bodyMedium: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+      ),
+      titleMedium: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+      ),
+      titleSmall: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 10,
+        fontWeight: FontWeight.w400,
+      ),
+      labelLarge: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+      ),
+      bodySmall: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+      ),
+      labelSmall: const TextStyle(
+        color: AppColors.aquaHaze,
+        fontSize: 10,
+        fontWeight: FontWeight.w400,
+      ),
+    ),
     inputDecorationTheme: const InputDecorationTheme(
       border: OutlineInputBorder(
         borderSide: BorderSide(
