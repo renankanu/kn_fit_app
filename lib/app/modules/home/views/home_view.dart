c8aaa55fe90f3c79519bbceedad1c0a1bdbba1ff
renankanu <renankanu@gmail.com>
2023-03-13 08:41:06 -0300

    feature: create home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,70 +8,71 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final statusBarHeight = MediaQuery.of(context).padding.top;
     return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.fromLTRB(20, 10 + statusBarHeight, 20, 0),
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Image.asset(
-                AppImages.icNotification,
-                height: 24,
-                color: AppColors.tiara,
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          Row(
-            children: const [
-              Text(
-                'Olá,',
-                style: TextStyle(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset(
+                  AppImages.icNotification,
+                  height: 24,
                   color: AppColors.tiara,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
                 ),
-              ),
-              Text(
-                'João',
-                style: TextStyle(
-                  color: AppColors.berylGreen,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: const [
+                Text(
+                  'Olá,',
+                  style: TextStyle(
+                    color: AppColors.tiara,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              ItemHome(
-                label: 'Treinos',
-                icon: AppImages.icBarbell,
-              ),
-              ItemHome(
-                label: 'Perfil',
-                icon: AppImages.icUser,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              ItemHome(
-                label: 'Favoritos',
-                icon: AppImages.icFavorite,
-              ),
-              ItemHome(
-                label: 'Avaliações',
-                icon: AppImages.icNotification,
-              ),
-            ],
-          )
-        ],
+                Text(
+                  ' Renan',
+                  style: TextStyle(
+                    color: AppColors.tiara,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                ItemHome(
+                  label: 'Treinos',
+                  icon: AppImages.icBarbell,
+                ),
+                ItemHome(
+                  label: 'Perfil',
+                  icon: AppImages.icUser,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                ItemHome(
+                  label: 'Favoritos',
+                  icon: AppImages.icFavorite,
+                ),
+                ItemHome(
+                  label: 'Avaliações',
+                  icon: AppImages.icNotification,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
