bf96e2e175ed1f4d04ad8dc11bdfa5d380c69e2c
renankanu <renankanu@gmail.com>
2023-03-12 21:13:59 -0300

    feature: create enum for gender

---
+++ lib/app/features/domain/entities/gender_enum.dart
@@ -0,0 +1,9 @@
+enum Gender {
+  female('Feminino'),
+  male('Masculino'),
+  other('Outro');
+
+  const Gender(this.description);
+
+  final String description;
+}
--- lib/app/features/domain/entities/student_entity.dart
+++ lib/app/features/domain/entities/student_entity.dart
@@ -1,14 +1,22 @@
 import 'package:equatable/equatable.dart';
 
+import 'gender_enum.dart';
+
 class StudentEntity extends Equatable {
   final String id;
   final String fullName;
+  final String? email;
+  final String? avatar;
+  final Gender gender;
 
   const StudentEntity({
     required this.id,
     required this.fullName,
+    this.email,
+    this.avatar,
+    this.gender = Gender.other,
   });
 
   @override
-  List<Object> get props => [id, fullName];
+  List<Object?> get props => [id, fullName, email, avatar, gender];
 }
